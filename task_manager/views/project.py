087070b06473336a79723c888fcafd41b4fd276d
lianqianyi <11156023@ntub.edu.tw>
2025-04-24 09:30:47 +0800

    修改 - project查詢結果為空產生的錯誤

--- task_manager/views/project.py
+++ task_manager/views/project.py
@@ -4,11 +4,16 @@ from task_manager.models.project import Project
 def main(request):
     context = {}
     project = Project.objects.all()
+    
+    project_list = []
+    
     for m in project:
         data = {}
         for field in m._meta.fields:
             field_name = field.name
             field_value = getattr(m, field_name)
             data[field_name] = field_value
-    context['project'] = data
+        project_list.append(data)
+        
+    context['project'] = project_list
     return render(request, 'project.html', context)
