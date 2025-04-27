499a04a23d0c7ecf36a1a330c9badec28f85be07
mebius <mebius.wei.zichen@gmail.com>
2025-04-27 17:05:58 +0800

    gantte 產出

--- project_system/urls.py
+++ project_system/urls.py
@@ -3,8 +3,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
-from django.shortcuts import redirect
+
+
 from task_manager.views import (
+    test,
     project,
     task_list,
     login,
@@ -13,22 +15,29 @@ from task_manager.views import (
     dynamic_search_member,
 )
 
-urlpatterns = [
-    path('project/', project.main, name="project"),
-    path('task/', task_list.main, name="task"),
-    path('login/', login.login_view, name="login"),
-    path('', RedirectView.as_view(url='/login/'), name="login"),
-    
-    # 認證相關路由
-    path('login/', login.login_view, name='login'),
-    path('register/', register.register_view, name='register'),
-    # path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-    
-    # 專案相關路由
-    path('project/', project.main, name='project'),
-    path('create_project/', create_project.main, name='create_project'),
-    path('dynamic_search_member/', dynamic_search_member.main, name='dynamic_search_member'),
-    
-    # 任務相關路由
-    path('task_list/', task_list.main, name='task_list'),
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = (
+    [
+        path("project/", project.main, name="project"),
+        path("task/", task_list.main, name="task"),
+        path("login/", login.login_view, name="login"),
+        path("", RedirectView.as_view(url="/login/"), name="login"),
+        
+        # 測試路由
+        path('test/', test.main, name='test'),
+        
+        # 認證相關路由
+        path('login/', login.login_view, name='login'),
+        path('register/', register.register_view, name='register'),
+        # path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+        
+        # 專案相關路由
+        path('project/', project.main, name='project'),
+        path('create_project/', create_project.main, name='create_project'),
+        path('dynamic_search_member/', dynamic_search_member.main, name='dynamic_search_member'),
+        
+        # 任務相關路由
+        path('task_list/', task_list.main, name='task_list'),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
+# fmt: on
--- task_manager/views/create_project.py
+++ task_manager/views/create_project.py
@@ -7,44 +7,39 @@ from task_manager.models.project_member import ProjectMember
 from django.contrib.auth.decorators import login_required
 from datetime import datetime, date
 
-@login_required(login_url='login')  # 確保用戶已登入
+
+@login_required(login_url="login")  # 確保用戶已登入
 def main(request):
     if request.method == "POST":
         projectName = request.POST.get("projectName")
         description = request.POST.get("description")
         dueDate = request.POST.get("dueDate")
-        user = User.objects.get(username = request.user)
+        user = User.objects.get(username=request.user)
         member_count = int(request.POST.get("member_count", "0"))
 
         if Project.objects.filter(name=projectName).exists():
             messages.warning(request, "專案名稱已存在")
-            return redirect('/project/')
+            return redirect("/project/")
 
         today = date.today()
         due_date_obj = datetime.strptime(dueDate, "%Y-%m-%d").date()
         if due_date_obj <= today:
             messages.warning(request, "截止日期必須在今天或之後")
-            return redirect('/project/')
-        
+            return redirect("/project/")
+
         # 創建新專案
         new_project = Project(
-            name=projectName,
-            description=description,
-            end_date=dueDate,
-            user_id=user
+            name=projectName, description=description, end_date=dueDate, user_id=user
         )
         new_project.save()
 
         for i in range(member_count):
             member_name = request.POST.get(f"member_name_{i}")
             member_email = request.POST.get(f"member_email_{i}")
-            user = User.objects.get(username = member_name, email = member_email)
-            project_member = ProjectMember(
-                project_id=new_project,
-                user_id=user
-            )
+            user = User.objects.get(username=member_name, email=member_email)
+            project_member = ProjectMember(project_id=new_project, user_id=user)
             project_member.save()
 
         messages.success(request, "專案創建成功")
-        return redirect('/project/')
-    return HttpResponseNotAllowed(['POST'])
+        return redirect("/project/")
+    return HttpResponseNotAllowed(["POST"])
