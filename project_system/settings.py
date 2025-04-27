d9c2de75aeec3e6c0267a6354d394da52e7e06fa
linjerry <linjerry102039@gmail.com>
2025-04-27 18:36:56 +0800

    上傳資料庫

--- project_system/settings.py
+++ project_system/settings.py
@@ -81,7 +81,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "task_manager/assets"),
-    os.path.join(BASE_DIR, "node_modules/"),
+    os.path.join(BASE_DIR, "task_manager/assets/node_modules"),
 ]
 
 
