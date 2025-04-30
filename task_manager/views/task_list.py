9b0795a64d620332af2c2749d0aa8dbf663bc1ed
linjerry <linjerry102039@gmail.com>
2025-04-30 14:47:30 +0800

    修改login頁面的顯示訊息

--- task_manager/views/task_list.py
+++ task_manager/views/task_list.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from task_manager.models.task import Task  # 導入您的Task模型
 from task_manager.models.project import Project  # 導入Project模型，用於顯示專案名稱
+from django.contrib.auth.decorators import login_required
 
-
+@login_required(login_url="login")
 def main(request):
     # 獲取所有任務
     tasks = Task.objects.all()
