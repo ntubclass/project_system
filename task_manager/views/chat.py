6ed2bd49f408eec28521f1c3a3ddfc3ff4f46826
mebius <mebius.wei.zichen@gmail.com>
2025-05-06 16:49:43 +0800

    message 修正

--- task_manager/views/chat.py
+++ task_manager/views/chat.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from task_manager.models.project import Project
 from task_manager.models.project_member import ProjectMember
-from task_manager.models.Message import Message
+from task_manager.models.message import Message
 from django.contrib.auth.decorators import login_required
 
 @login_required(login_url="login")
