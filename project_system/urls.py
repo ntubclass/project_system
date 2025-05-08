5e63b9c8f9ee775765d3757442b6fea9a533983b
Ray <raygoodgo@gmail.com>
2025-05-08 17:54:16 +0800

    修正

--- project_system/urls.py
+++ project_system/urls.py
@@ -50,10 +50,10 @@ urlpatterns = (
         path('delete_file/<int:project_id>/', delete_file.main, name='delete_file'),
         path('upload_avatar_api/', upload_avatar_api.main, name='upload_avatar_api'), 
         path('project_detail/<int:project_id>/', project_detail.main, name='project_detail'),
-        path('forgot-password/', forgot_password.forgot_password_view, name='forgot_password'),
-        path('verify-code/', verify_code.verify_code_view, name='verify_code'),
-        path('resend-code/', verify_code.resend_code_view, name='resend_code'),
-        path('reset-password/', reset_password.reset_password_view, name='reset_password'),   
+        path('forgot_password/', forgot_password.forgot_password_view, name='forgot_password'),
+        path('verify_code/', verify_code.verify_code_view, name='verify_code'),
+        path('resend_code/', verify_code.resend_code_view, name='resend_code'),
+        path('reset_password/', reset_password.reset_password_view, name='reset_password'),   
     ]
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 )
