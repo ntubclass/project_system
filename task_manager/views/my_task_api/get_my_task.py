11e0db8eac27339b78a742cbf6609de8d350a214
lianqianyi <11156023@ntub.edu.tw>
2025-05-15 10:04:56 +0800

    resolve merge

--- task_manager/views/my_task_api/get_my_task.py
+++ task_manager/views/my_task_api/get_my_task.py
@@ -1,18 +1,29 @@
 from task_manager.models.task import Task  
+from task_manager.models.task_member import TaskMember  
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from task_manager.models.user_info import UserInfo
+from django.db.models import Q
 
 @login_required(login_url="login")
-def main(request, project_id):
+def main(request):
     if request.method == "GET":
-        tasks = Task.objects.filter(project_id=project_id)
+        # Get tasks created by the user
+        created_tasks = Task.objects.filter(user_id=request.user)
+        
+        # Get tasks where the user is a member
+        member_task_ids = TaskMember.objects.filter(user_id=request.user).values_list('task_id', flat=True)
+        member_tasks = Task.objects.filter(task_id__in=member_task_ids)
+        
+        # Combine both querysets and remove duplicates
+        all_tasks = created_tasks | member_tasks
+        all_tasks = all_tasks.distinct()
 
         context = {
             'tasks_data': []
         }
 
-        for task in tasks:
+        for task in all_tasks:
             task_data = {
                 "id": task.task_id,
                 "name": task.name,
@@ -26,3 +37,4 @@ def main(request, project_id):
             context['tasks_data'].append(task_data)
 
         return JsonResponse(context)
+    
