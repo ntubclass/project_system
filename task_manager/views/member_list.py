e6e3c263eff2b4b60e8a749091b33d5dc4217a0e
11046083 <11046083@ntub.edu.tw>
2025-05-15 16:19:35 +0800

    搜尋專案成員(尚未抓資料庫)

--- task_manager/views/member_list.py
+++ task_manager/views/member_list.py
@@ -3,11 +3,18 @@ from django.shortcuts import render
 def main(request, project_id):
     # 模擬 7 位成員，其中第一位是專案擁有者
     members = [
-        {"name": "張三", "email": "123@gmail.com", "role": "專案擁有者"},
+        {"name": "李", "email": "3434@gmail.com", "role": "專案擁有者"},
     ] + [
-        {"name": "張三", "email": "123@gmail.com", "role": "專案成員"}
-        for _ in range(6)
-    ]
+        {"name": "魏", "email": "2222@gmail.com", "role": "專案成員"}
+    ] + [
+        {"name": "林", "email": "9999@gmail.com", "role": "專案成員"},
+    ] + [
+        {"name": "潘", "email": "1919@gmail.com", "role": "專案成員"},
+    ] + [
+        {"name": "連", "email": "2323@gmail.com", "role": "專案成員"},
+    ] + [
+        {"name": "ABC", "email": "888@gmail.com", "role": "專案成員"},
+    ] 
 
     context = {
         "project_id": project_id,
