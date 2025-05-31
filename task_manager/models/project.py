ecac2f50ea4e0879a6fb4078883a8e489a637e71
linjerry <linjerry102039@gmail.com>
2025-05-31 16:44:47 +0800

    上傳測試資料

--- task_manager/models/project.py
+++ task_manager/models/project.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Project(models.Model):
     project_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    start_date = models.DateTimeField(auto_now_add=True)
+    start_date = models.DateTimeField()
     end_date = models.DateTimeField()
     description = models.TextField(blank=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
