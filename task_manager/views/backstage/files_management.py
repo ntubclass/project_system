baad836983abd949e750a8ed30adcca9e078f384
linjerry <linjerry102039@gmail.com>
2025-06-04 16:39:01 +0800

    檔案管理功能修正

--- task_manager/views/backstage/files_management.py
+++ task_manager/views/backstage/files_management.py
@@ -4,9 +4,13 @@ from task_manager.utils import hum_convert
 from task_manager.utils import get_file_icon
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.db.models import Q
 
 @login_required(login_url="login")
 def main(request):
+
+    search_query = request.GET.get('search', '').strip()
+
     if not request.user.is_superuser:
         messages.error(request, "沒有權限查看此頁面")
         return redirect('project')
@@ -16,6 +20,16 @@ def main(request):
     }
 
     files = File.objects.all()
+
+    if search_query:
+        files = files.filter(
+            Q(file_name__icontains=search_query) |
+            Q(file_type__icontains=search_query) |
+            Q(user_id__username__icontains=search_query) |
+            Q(user_id__first_name__icontains=search_query) |
+            Q(user_id__last_name__icontains=search_query)
+        )
+
     for m in files:
         data = {}
         for field in m._meta.fields:
--- task_manager/views/file_api/files.py
+++ task_manager/views/file_api/files.py
@@ -1,18 +1,32 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from task_manager.models.project import Project
+from django.db.models import Q
 from task_manager.models.file import File
 from task_manager.utils import hum_convert
 from task_manager.utils import get_file_icon
 
 @login_required(login_url="login")
 def main(request, project_id):
+    # 取得搜尋參數
+    search_query = request.GET.get('search', '').strip()
+    
     context = {
         "project_id": project_id,
         "file_data": [],
     }
 
     files = File.objects.filter(project_id=project_id)
+    
+    # 如果有搜尋關鍵字，進行後端搜尋
+    if search_query:
+        files = files.filter(
+            Q(file_name__icontains=search_query) |
+            Q(file_type__icontains=search_query) |
+            Q(user_id__username__icontains=search_query) |
+            Q(user_id__first_name__icontains=search_query) |
+            Q(user_id__last_name__icontains=search_query)
+        )
+    
     for m in files:
         data = {}
         for field in m._meta.fields:
@@ -26,6 +40,13 @@ def main(request, project_id):
             elif field_name == "file_type":
                 field_value = field_value.split("/")[1]
             data[field_name] = field_value
+        
+        # 處理用戶名稱顯示
+        if hasattr(m.user_id, 'first_name') and m.user_id.first_name:
+            data["user_id"] = f"{m.user_id.first_name} {m.user_id.last_name or ''}".strip()
+        else:
+            data["user_id"] = m.user_id.username
+            
         file_info = get_file_icon.main(m.file_name)
         data["class_icon"] = file_info['icon']
         data["class_bgClass"] = file_info['bgClass']   
